67dbd8ea26678ba3c4525c04069fb84ac6637da8
pb <priyajitbiswal91@gmail.com>
2024-05-30 02:00:20 +0530

    first commit

---
+++ temp.py
@@ -0,0 +1,6 @@
+def format_output(x, spec):
+    return f"{x:{spec}}"
+x = 1234.56789
+print(format_output(x, '0.2f'))  # Output: 1234.56
+print(format_output(x, '>10.1f'))  # Output:    1234.5
+print(format_output(x, '<10.1f')) 
